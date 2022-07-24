b6c4f22b89602cb47020b6ada5c67256e7c7811b
sveta <svetlana.fedoseeva.88@mail.ru>
2022-07-24 12:34:16 +0300

    первый коммит

---
+++ src/main/java/pro/sky/skyprospringhw11/SkyproSpringHw11Application.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringhw11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringHw11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringHw11Application.class, args);
+	}
+
+}
